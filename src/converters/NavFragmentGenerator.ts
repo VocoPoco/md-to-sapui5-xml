4ceb7b20c153058adc54f0bc35f5d92bd5d3152c
VocoPoco <vik.penev@gmail.com>
2025-04-30 14:22:14 +0300

    fix: changed import from @src to ../

--- src/converters/NavFragmentGenerator.ts
+++ src/converters/NavFragmentGenerator.ts
@@ -1,4 +1,4 @@
-import XML_TEMPLATE from '@src/templates/xmlTemplate';
+import XML_TEMPLATE from '../templates/xmlTemplate';
 import NavFragmentHandler from './processors/NavFragmentHandler';
 
 /**
